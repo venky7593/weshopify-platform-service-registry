e10308f015e11f6f2652f015288a40fb34d82d77
Narsi-Myteaching <104019637+Narsi-Myteaching@users.noreply.github.com>
2023-05-03 01:01:27 +0530

    Update WeshopifyPlatformServicesRegistryApplication.java

--- src/main/java/com/weshopify/platform/WeshopifyPlatformServicesRegistryApplication.java
+++ src/main/java/com/weshopify/platform/WeshopifyPlatformServicesRegistryApplication.java
@@ -1,10 +1,11 @@
 package com.weshopify.platform;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SystemMetricsAutoConfiguration.class)
 @EnableEurekaServer
 public class WeshopifyPlatformServicesRegistryApplication {
 
